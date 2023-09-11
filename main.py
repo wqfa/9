10f473f175265f7702f4161cefbecff65dd56877
XF <118357460+wqfa@users.noreply.github.com>
2023-09-12 00:20:08 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,68 @@
+import telebot,time
+from telebot import types
+TOKEN = '6407894610:AAHTe6oWh1tk1CssOUBv44cmkcK3C42AEeY'
+bot = telebot.TeleBot(TOKEN)
+@bot.message_handler(commands=['start'])
+def start(message):
+	a = types.InlineKeyboardButton('افضل بوتات التيليكرام 🤖🔥', url='t.me/DEVEVII')
+	b = types.InlineKeyboardButton('اضافة الى مجموعه - add to group', url='https://t.me/nl60bot?startgroup=true')
+	c = types.InlineKeyboardMarkup()
+	c.add(b)
+	c.add(a)
+	bot.send_message(message.chat.id,text=f'''<strong>
+اهلا بك عزيزي 👤.
+انا هو بوت تيليكرام 🤖،
+وضيفتي هي منع السورسات والنشر التلقائي داخل المجموعات 🤖
+
+اضفني الى المجموعه ثم ارفعني مشرف
+
+المبرمج : @C15CS
+</strong>
+	''', parse_mode='html', reply_markup=c)
+def check_message(message):
+	text = message.text.lower()
+	if ".تكرار" in text and len(text) > 2 or ".مكرر" in text and len(text) > 2 or ".مؤقت" in text and len(text) > 2 or ".موقت" in text and len(text) > 2 or ".الاوامر" in text and len(text) > 2 or ".فحص" in text and len(text) > 2 or ".كتم" in text and len(text) > 2 or ".وقتي" in text and len(text) > 2 or ".حظر" in text and len(text) > 2 or ".سورس" in text and len(text) > 2 or ".بوت" in text and len(text) > 2 or ".سورسي" in text and len(text) > 2 or ".كت" in text and len(text) > 2 or ".اوامر" in text and len(text) > 2 or ".اوامري" in text and len(text) > 2 or ".منع" in text and len(text) > 2 or ".موقت" in text and len(text) > 2:
+		return True
+	return False
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+	if check_message(message):
+		last_name = message.from_user.full_name
+		username = message.from_user.username
+		id = message.from_user.id
+		photos = bot.get_user_profile_photos(id).photos
+		photo_file_id = None
+		if photos:
+			photo_file_id = photos[0][-1].file_id
+		dev = types.InlineKeyboardButton('pro 👨‍💻', url='t.me/C15cs')
+		bb = types.InlineKeyboardMarkup(row_width=1);bb.add(dev)
+		url = f'https://t.me/{username}'
+		bot.send_photo(chat_id=message.chat.id, photo=photo_file_id, caption=f'''
+❖ - السورسات ممنوعه هنا 🎭
+
+تم تقييد العضو {last_name} لانه خالف القوانين !
+
+معلومات العضو المخالف !
+
+[⌯] name ⌯ {last_name}
+[⌯] user ⌯ @{username}
+[⌯] id ⌯ {id}
+
+مده التقييد نصف ساعه غير قابلة للتغير!
+    ''', reply_markup=bb)
+		chat_group = message.chat.id
+		try:
+			bot.restrict_chat_member(chat_group, id, can_send_messages=False)
+		except:
+			bot.send_message(chat_group,text='لا يمكنني تقييد مشرف !')
+		time.sleep(1800)
+		try:
+			bot.restrict_chat_member(chat_group, id, can_send_messages=True)
+			bot.send_message(chat_group,text=f'''
+المستخدم {last_name} ، @{username}
+تم فك التقيد عنك👤
+			''')
+		except:
+			bot.send_message(chat_group,text=f'لا استطيع فك تقيد العضو @{username}')
+
+bot.polling(True)
